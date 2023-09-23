8556ce75df32acef3bcd43ffb9319efcb97199a1
aakash-sharma25 <as024harma@gmail.com>
2023-09-23 22:11:25 +0530

    changed website name

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'GYM-APP',
+  title: 'The-Fitness-Hub',
   description: 'This is a GYM website built using Next JS and Tailwing CSS',
 }
 
